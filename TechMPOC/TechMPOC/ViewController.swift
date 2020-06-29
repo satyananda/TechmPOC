1cbf13154c2cf296ae40d0173d53fa7f4622bf42
Satyanand <satyanand.t@gmail.com>
2020-06-29 13:08:27 +0530

    initial commit

---
+++ TechMPOC/TechMPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechMPOC
+//
+//  Created by Satyananda Kumar on 29/06/20.
+//  Copyright © 2020 Satyananda Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TechMPOC/TechMPOCTests/TechMPOCTests.swift
@@ -0,0 +1,34 @@
+//
+//  TechMPOCTests.swift
+//  TechMPOCTests
+//
+//  Created by Satyananda Kumar on 29/06/20.
+//  Copyright © 2020 Satyananda Kumar. All rights reserved.
+//
+
+import XCTest
+@testable import TechMPOC
+
+class TechMPOCTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
